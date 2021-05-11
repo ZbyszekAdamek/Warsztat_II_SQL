279c08e64c48d8626dedcfa10018ddc0de463acf
ZbyszekAdamek <zbigniew.t.adamek@gmail.com>
2021-05-11 20:55:57 +0200

    Commit kolejne zmiany

--- src/main/java/pl/coderslab/entity/UserDao.java
+++ src/main/java/pl/coderslab/entity/UserDao.java
@@ -6,27 +6,43 @@ import java.sql.*;
 
 public class UserDao {
 
+    private static final String CREATE_USER_QUERY =
+            "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+
+    private static final String DELETE_USER_QUERY = "DELETE FROM users where id = ?";
+    private static final String UPDATE_USER_QUERY = null;
 
 
-//    public User create(User user) {
-//        try (Connection conn = DbUtil.getConnection()) {
-//            PreparedStatement statement =
-//                    conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
-//            statement.setString(1, user.getUserName());
-//            statement.setString(2, user.getEmail());
-//            statement.setString(3, hashPassword(user.getPassword()));
-//            statement.executeUpdate();
-//            //Pobieramy wstawiony do bazy identyfikator, a następnie ustawiamy id obiektu user.
-//            ResultSet resultSet = statement.getGeneratedKeys();
-//            if (resultSet.next()) {
-//                user.setId(resultSet.getInt(1));
-//            }
-//            return user;
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//            return null;
-//        }
-//    }
+
+    public User create(User user) {
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement statement =
+                    conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getUserName());
+            statement.setString(2, user.getEmail());
+/*            statement.setString(3, hashPassword(user.getPassword()));*/
+            statement.executeUpdate();
+            //Pobieramy wstawiony do bazy identyfikator, a następnie ustawiamy id obiektu user.
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                user.setId(resultSet.getInt(1));
+            }
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+/*    public void delete(int userId){
+        try (Connection connection = DbUtil.getConnection();
+            PreparedStatement statement =
+                    connection.prepareStatement(DELETE_USER_QUERY.replace("tableName", tableName));) {
+            statement.setInt(1, userId);
+            statement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }*/
 //    public User read(int userId){
 //
 //    }
@@ -36,8 +52,13 @@ public class UserDao {
 //    public User[] findAll(){
 //
 //    }
-public void delete(int userId){
+
+
 
 }
+/*    public String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }*/
 
-}
+
+
